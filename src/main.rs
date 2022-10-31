38f18d936382ea8277c8c286a8f1c98ba9250c48
demiurge94 <erickvicencio@gmail.com>
2022-10-30 17:31:57 -0700

    tokio use

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
-fn main() {
-    println!("Hello, world!");
-    println!("I'm an idiot"); 
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet_and_insult(req: HttpRequest) -> impl Responder{
+    let name = req.match_info().get("name").unwrap_or("world");
+    let insult = req.match_info().get("insult").unwrap_or("IDIOT");
+    format!("Hello {} \nI am an{} ! ", &name, &insult)
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet_and_insult))
+            .route("/{name}", web::get().to(greet_and_insult))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
